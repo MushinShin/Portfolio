9b3ca44ac22df80b6299e430949fe5becb6499ea
FacuFrancia <facundofrancia3@hotmail.com>
2021-11-08 09:13:35 -0300

    styles changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,7 +5,7 @@ import "../styles/heroStyles.css";
 const Hero = () => {
   return (
     <section className="block block--dark block--skewed-left">
-      <div className="grid grid--1x2">
+      <div className="container_hero grid grid--1x2">
         <header className="block__header hero__content">
           <h1 className="block__heading">Hi! Welcome To my Portfolio</h1>
           <p className="hero__tagline">Facundo Francia</p>
