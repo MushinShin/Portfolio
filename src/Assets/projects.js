8d5f4c7d624c074249e14738d6322650b5be1558
FacuFrancia <facundofrancia3@hotmail.com>
2021-11-10 17:00:37 -0300

    swiper logic breakpoints and styles

--- src/Assets/projects.js
+++ src/Assets/projects.js
@@ -2,7 +2,49 @@ import facufy from "../imgs/Facufy.png";
 
 const projects = [
   {
-    // id: uuidv4(),
+    id: "uuidv4",
+    name: "Facufy",
+    desc: `A template for a cloud hosting web page,
+     made with mobile first and pixel perfect philosophy`,
+    img: facufy,
+  },
+  {
+    id: "uuidv5",
+    name: "Facufy",
+    desc: `A template for a cloud hosting web page,
+     made with mobile first and pixel perfect philosophy`,
+    img: facufy,
+  },
+  {
+    id: "uuidv5",
+    name: "Facufy",
+    desc: `A template for a cloud hosting web page,
+     made with mobile first and pixel perfect philosophy`,
+    img: facufy,
+  },
+  {
+    id: "uuidv5",
+    name: "Facufy",
+    desc: `A template for a cloud hosting web page,
+     made with mobile first and pixel perfect philosophy`,
+    img: facufy,
+  },
+  {
+    id: "uuidv5",
+    name: "Facufy",
+    desc: `A template for a cloud hosting web page,
+     made with mobile first and pixel perfect philosophy`,
+    img: facufy,
+  },
+  {
+    id: "uuidv5",
+    name: "Facufy",
+    desc: `A template for a cloud hosting web page,
+     made with mobile first and pixel perfect philosophy`,
+    img: facufy,
+  },
+  {
+    id: "uuidv5",
     name: "Facufy",
     desc: `A template for a cloud hosting web page,
      made with mobile first and pixel perfect philosophy`,
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
-import { Swiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
 import projects from "../Assets/projects";
 import ProjectItem from "./projectItem";
+import "../styles/projects.css";
+import "swiper/swiper-bundle.min.css";
+
+SwiperCore.use([Navigation]);
 
 const Project = () => {
   return (
-    <div>
+    <div className="projectSectionStyle">
       <div className="container">
         <h1>Projects</h1>
         <div className="projects_allItems">
-          <Swiper>
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={1}
+            navigation
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              },
+
+              768: {
+                slidesPerView: 2,
+              },
+
+              1200: {
+                slidesPerView: 3,
+              },
+            }}
+          >
             {projects.map((project, index) => {
               if (index >= 5) return;
-              return <ProjectItem />;
+              return (
+                <SwiperSlide key={project.id}>
+                  <ProjectItem />
+                </SwiperSlide>
+              );
             })}
           </Swiper>
         </div>
