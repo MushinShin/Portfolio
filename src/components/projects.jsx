ed60e781f4e2a331199948391a374e9ae85008ee
FacuFrancia <facundofrancia3@hotmail.com>
2021-11-19 10:59:23 -0300

    styles, react fade font changes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation } from "swiper";
+import { Fade } from "react-fade";
 import projects from "../Assets/projects";
 import ProjectItem from "./projectItem";
 import "../styles/projects.css";
@@ -10,46 +11,46 @@ SwiperCore.use([Navigation]);
 
 const Project = () => {
   return (
-    <div className="projectSectionStyle">
-      <div className="container">
-        <h1 className="Project-Section">Projects</h1>
-        <div className="projects_allItems">
-          <Swiper
-            className="swiperItem"
-            spaceBetween={30}
-            slidesPerView={1}
-            navigation
-            breakpoints={{
-              640: {
-                slidesPerView: 1,
-              },
-
-              768: {
-                slidesPerView: 2,
-              },
-
-              1200: {
-                slidesPerView: 3,
-              },
-            }}
-          >
-            {projects.map((project, index) => {
-              if (index >= 3) return;
-              return (
-                <SwiperSlide key={project.id}>
-                  <ProjectItem
-                    title={project.name}
-                    desc={project.desc}
-                    img={project.img}
-                    link={project.link}
-                  />
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
+    <Fade>
+      <div className="projectSectionStyle">
+        <div className="container">
+          <h1 className="Project-Section">Projects</h1>
+          <div className="projects_allItems">
+            <Swiper
+              className="swiperItem"
+              spaceBetween={30}
+              slidesPerView={1}
+              navigation
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                },
+                768: {
+                  slidesPerView: 2,
+                },
+                1200: {
+                  slidesPerView: 3,
+                },
+              }}
+            >
+              {projects.map((project, index) => {
+                if (index >= 3) return;
+                return (
+                  <SwiperSlide key={project.id}>
+                    <ProjectItem
+                      title={project.name}
+                      desc={project.desc}
+                      img={project.img}
+                      link={project.link}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
-    </div>
+    </Fade>
   );
 };
 
