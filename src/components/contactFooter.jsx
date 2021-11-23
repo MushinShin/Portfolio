8bc135eaff01f3cc2fa8e77ce8dac610d4336bf1
FacuFrancia <facundofrancia3@hotmail.com>
2021-11-23 11:35:52 -0300

    footer component and stlyes

---
+++ src/components/contactFooter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const FooterCont = () => {
+  return (
+    <div>
+      <h1>This is footer col</h1>
+    </div>
+  );
+};
+
+export default FooterCont;
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import FooterCont from "./contactFooter";
+import "../styles/footerStyles.css";
 
 const Footer = () => {
   return (
     <div>
-      <div className="container"></div>
+      <div className="containerFooter">
+        <div className="footer_col1">
+          <h1 className="footer_col1_title">Facundo Francia</h1>
+        </div>
+        <div className="footer_col2">
+          <FooterCont />
+        </div>
+        <div className="footer_col3">
+          <FooterCont />
+        </div>
+        <div className="footer_col4">
+          <FooterCont />
+        </div>
+      </div>
     </div>
   );
 };
