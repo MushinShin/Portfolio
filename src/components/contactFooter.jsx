f7ac824a48f91b96a27778c3cce0cc05b07f40bb
FacuFrancia <facundofrancia3@hotmail.com>
2021-12-03 10:04:36 -0300

    footer

--- src/components/contactFooter.jsx
+++ src/components/contactFooter.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
-const FooterCont = () => {
+const FooterCont = ({
+  heading = "col_heading",
+  links = [
+    {
+      type: "Link",
+      title: "Home",
+      path: "/home",
+    },
+    {
+      type: "Link",
+      title: "About",
+      path: "/about",
+    },
+  ],
+}) => {
   return (
     <div>
-      <h1>This is footer col</h1>
+      <h1>{heading}</h1>
+      <ul>
+          {
+              links.map((item, index) => {
+                  <li key={index}>
+                      {item.type === 'Link' ? {
+                          
+                      }}
+                  </li>
+              })
+          }
+      </ul>
     </div>
   );
 };
